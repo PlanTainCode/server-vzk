e8af10aeae76d6ba3facf255f520c5b464ae2aaa
PlanTainCode <plancode14@gmail.com>
2022-11-18 18:48:44 +0300

    main

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'vzk'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
+      password: env('DATABASE_PASSWORD', '3141'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
